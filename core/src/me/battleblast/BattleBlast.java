1c907518beec7a82a32a9400384078f9e95f2d23
A.Nackov <anackov@gmail.com>
2018-06-02 23:00:20 +0300

    Close XYZ - extract GameInputHandler class
    extract in a new class for better readability

--- core/src/me/battleblast/BattleBlast.java
+++ core/src/me/battleblast/BattleBlast.java
@@ -2,8 +2,6 @@ package me.battleblast;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -42,56 +40,9 @@ public class BattleBlast extends ApplicationAdapter {
 
         batch = new SpriteBatch();
 
-        Gdx.input.setInputProcessor(new InputAdapter() {
-            @Override
-            public boolean keyUp(int keycode) {
-                float movement = 0;
-                switch(keycode) {
-                    case Keys.LEFT:
-                        player.setRotation(270);
-                        movement = player.getX() - 1000 * Gdx.graphics.getDeltaTime();
-                        if (movement > 0) {
-                            player.setX(movement);
-                        } else {
-                            player.setX(0);
-                        }
-                        break;
-                    case Keys.RIGHT:
-                        player.setRotation(90);
-                        movement = player.getX() + 1000 * Gdx.graphics.getDeltaTime();
-                        if (movement < Gdx.graphics.getWidth() - player.getWidth()) {
-                            player.setX(movement);
-                        } else {
-                            player.setX(Gdx.graphics.getWidth() - player.getWidth());
-                        }
-                        break;
-                    case Keys.UP:
-                        player.setRotation(180);
-                        movement = player.getY() + 1000 * Gdx.graphics.getDeltaTime();
-                        if (movement < Gdx.graphics.getHeight() - player.getHeight()) {
-                            player.setY(movement);
-                        } else {
-                            player.setY(Gdx.graphics.getHeight() - player.getHeight());
-                        }
-                        break;
-                    case Keys.DOWN:
-                        player.setRotation(0);
-                        movement = player.getY() - 1000 * Gdx.graphics.getDeltaTime();
-                        if (movement > 0) {
-                            player.setY(movement);
-                        } else {
-                            player.setY(0);
-                        }
-                        break;
-                }
-                return false;
-            }
-
-            @Override
-            public boolean keyDown(int keycode) {
-                return false;
-            }
-        });
+        GameInputHandler inputHandler = new GameInputHandler();
+        inputHandler.setPlayer(player);
+        Gdx.input.setInputProcessor(inputHandler);
     }
 
     @Override
