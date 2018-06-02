98d7c891a05baffd9671103713d61c89921ab219
A.Nackov <anackov@gmail.com>
2018-06-02 19:21:33 +0300

    Close #something - render tiled map

--- desktop/src/me/battleblast/desktop/DesktopLauncher.java
+++ desktop/src/me/battleblast/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import me.battleblast.BattleBlast;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 640; // 20 tiles on map, each 32 pixels wide
+        config.height = 640; // 20 tiles on map, each 32 pixels wide
 		new LwjglApplication(new BattleBlast(), config);
 	}
 }
