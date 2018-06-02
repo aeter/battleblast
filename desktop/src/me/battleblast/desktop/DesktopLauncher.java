76f17a0477b62d2cc193997f318b6d5f5d868d9f
A.Nackov <anackov@gmail.com>
2018-06-02 17:42:00 +0300

    Close #XYZ - initial code
    Description: after running `java -jar gdx-setup.jar`

---
+++ desktop/src/me/battleblast/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.battleblast.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.battleblast.BattleBlast;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BattleBlast(), config);
+	}
+}
